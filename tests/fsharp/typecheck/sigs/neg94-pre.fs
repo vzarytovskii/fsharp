e32abd327be379f98994f800eac6634cc4283a2b
Don Syme <dsyme@microsoft.com>
2020-01-31 17:51:05 +0000

    adjust test case

--- tests/fsharp/typecheck/sigs/neg94-pre.fs
+++ tests/fsharp/typecheck/sigs/neg94-pre.fs
@@ -4,7 +4,7 @@ open System
 
 type Class1() =
   static member inline ($) (r:'R, _) = fun (x:'T) -> ((^R) : (static member method2: ^T -> ^R) x)
-  static member inline method1 x = Unchecked.defaultof<'r> $ Class1()
+  static member inline method1 (x:'X) : (_ -> 'r) = op_Dollar Unchecked.defaultof<'r> (Class1())
 
 type IComm = 
     [<CLIEvent>]
